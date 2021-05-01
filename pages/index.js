0b1f2c413c737f11536bcf704c734af03653d200
aman0207 <aman27verma.mohit@gmail.com>
2021-05-01 10:55:06 +0530

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Hello</title>
+        <meta name="keywords" content="web,material" />
+      </Head>
+      <h1>Hello World!</h1>
+    </>
+  );
+}
