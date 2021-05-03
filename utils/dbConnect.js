7668641976b2c6f2412fd64ad4f5ef98f0ea7e70
Dev Vrat Singh <itsdvvs.18@gmail.com>
2021-05-03 22:39:09 +0530

    Created function to load variables based on environment

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
+import DotENV from "dotenv";
 
+import { getConfigFilePath } from "../utils/functions";
+
+// configuring the environment variables.
+DotENV.config({ path: getConfigFilePath() });
 const connection = {};
 
 async function dbConnect() {
-  if (connection.isConnected) {
-    return;
-  }
+  if (connection.isConnected) return;
   console.log("DB connection URL : " + process.env.MONGO_URL);
 
   const db = await mongoose.connect(process.env.MONGO_URL, {
