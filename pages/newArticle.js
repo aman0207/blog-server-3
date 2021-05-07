5c2b00edd09ce7ecbce44a261e4470720b4b9324
aman0207 <aman27verma.mohit@gmail.com>
2021-05-07 21:48:43 +0530

    paper added

--- pages/newArticle.js
+++ pages/newArticle.js
@@ -5,25 +5,28 @@ import Grid from "@material-ui/core/Grid";
 import Hidden from "@material-ui/core/Hidden";
 
 import ArticleForm from "../components/ArticleForm";
+import { Paper } from "@material-ui/core";
 
 const NewArticlePage = () => {
   return (
-    <Grid container spcaing={2}>
-      {/* Offset(left), only on medium and above screen sizes */}
-      <Hidden smDown>
-        <Grid item md={1} lg={3}></Grid>
-      </Hidden>
+    <Paper>
+      <Grid container spcaing={2}>
+        {/* Offset(left), only on medium and above screen sizes */}
+        <Hidden smDown>
+          <Grid item md={1} lg={3}></Grid>
+        </Hidden>
 
-      {/* Editor Container */}
-      <Grid item xs={12} md={10} lg={6}>
-        <ArticleForm />
+        {/* Editor Container */}
+        <Grid item xs={12} md={10} lg={6}>
+          <ArticleForm />
+        </Grid>
+
+        {/* Offset(right), only on medium and above screen sizes */}
+        <Hidden smDown>
+          <Grid item md={1} lg={3}></Grid>
+        </Hidden>
       </Grid>
-
-      {/* Offset(right), only on medium and above screen sizes */}
-      <Hidden smDown>
-        <Grid item md={1} lg={3}></Grid>
-      </Hidden>
-    </Grid>
+    </Paper>
   );
 };
 
